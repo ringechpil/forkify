e375143fe555fdbb7e6c3f2e1a28826738c35c18
ringechpil <alegzander88@gmail.com>
2021-01-20 10:11:37 +0100

    Initial commi

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = `https://forkify-api.herokuapp.com/api/v2/recipes/`;
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = `e4e4dc46-01b6-4913-9376-85cc81021dc0`;
+export const MODAL_CLOSE_SEC = 2.5;
